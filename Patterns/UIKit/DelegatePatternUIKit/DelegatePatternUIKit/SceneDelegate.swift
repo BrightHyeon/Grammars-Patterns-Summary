4ee7a278fd5dbe71cf6a53a85d596950d52eb673
BrightHyeon <swiftist9891@gmail.com>
2022-03-10 15:39:47 +0900

    Storyboard없이 codebase로 windowScene 구성

--- Patterns/UIKit/DelegatePatternUIKit/DelegatePatternUIKit/SceneDelegate.swift
+++ Patterns/UIKit/DelegatePatternUIKit/DelegatePatternUIKit/SceneDelegate.swift
@@ -13,40 +13,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        //MARK: - 선택사항
+//        window?.tintColor = .label                          //* tintColor프로퍼티를 가지는 UIView객체들의 기본 tint색 설정가능
+        //MARK: - Codebase로 App을 만들때, 필수 Window설정 3박자.
+        window?.backgroundColor = .systemBackground                             //1. 배경색
+        window?.rootViewController = UINavigationController(rootViewController: GetViewController())    //2. 루트뷰 지정
+        window?.makeKeyAndVisible()                                             //3. 실제로 보이도록 나타나도록 설정(마무리 멘트)
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
 
