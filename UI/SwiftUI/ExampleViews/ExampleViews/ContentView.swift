197749d4831ce6d76f48b8490aabe0c9953025f6
BrightHyeon <swiftist9891@gmail.com>
2022-03-22 18:54:39 +0900

    SwiftUI에 있는 다양한 형태의 Views를 그려볼 프로젝트 생성

---
+++ UI/SwiftUI/ExampleViews/ExampleViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExampleViews
+//
+//  Created by HyeonSoo Kim on 2022/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UI/SwiftUI/ExampleViews/ExampleViews/ExampleViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleViewsApp.swift
+//  ExampleViews
+//
+//  Created by HyeonSoo Kim on 2022/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
