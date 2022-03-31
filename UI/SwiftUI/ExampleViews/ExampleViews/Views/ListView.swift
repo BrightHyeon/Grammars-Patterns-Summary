1ceeb835a577e3b5e91b320f7ab019d1f497afda
BrightHyeon <swiftist9891@gmail.com>
2022-03-31 19:11:13 +0900

    List tutorial study file

--- UI/SwiftUI/ExampleViews/ExampleViews/Views/ListView.swift
+++ UI/SwiftUI/ExampleViews/ExampleViews/Views/ListView.swift
@@ -18,30 +18,55 @@ struct ListView: View { //MARK: SwiftUI 도서 250page부근 참고.
     @State private var toggleStatus: Bool = true
     
     var body: some View {
-        List {
-            
-            Section {
-                Toggle(isOn: $toggleStatus) {
-                    Text("Allow Notifications")
-                }
-            } header: {
-                Text("Settings")
-            }
-            
-            Section {
-                ForEach(listData) { item in
-                    HStack {
-                        Image(systemName: item.imageName)
-                        Text(item.task)
+        NavigationView {
+            List {
+                
+                Section {
+                    Toggle(isOn: $toggleStatus) {
+                        Text("Allow Notifications")
                     }
+                } header: {
+                    Text("Settings")
                 }
-            } header: {
-                Text("To Do Tasks")
+                
+                Section {
+                    ForEach(listData) { item in
+                        HStack {           //View타입은 다 올수있다.(목적지)
+                            NavigationLink(destination: Text(item.task)) {
+                                Image(systemName: item.imageName)
+                                Text(item.task)
+                            }
+                        }
+                    }
+                    //MARK: 편집을 위한 사전작업(수정자를 셀에 적용시키기)
+                    .onDelete(perform: deleteItem)
+                    .onMove(perform: moveItem)
+                    //둘다 perform함수까지 잘 넣어줘야 활성화됨.
+                } header: {
+                    Text("To Do Tasks")
+                }
             }
+            .navigationTitle(Text("To Do List"))
+            .navigationBarTitleDisplayMode(.large)
+            .navigationBarItems(trailing: EditButton())
+            //MARK: onMove와 EditButton()은 한 세트. 편집 모드로 알아서 전환하게함.
         }
+    }
+    
+    //MARK: 편집 기능
+    //삭제
+    func deleteItem(at offsets: IndexSet) {
+        //데이터 소스에서 항목을 삭제하는 코드가 여기온다.
+        //실제 데이터가 담긴 곳에서도 데이터를 삭제해야한단 것!
+    }
+    //이동
+    func moveItem(from source: IndexSet, to destination: Int) {
+        //항목을 재배열하는 코드가 온다.
     }
 }
 
+
+//MARK: Storage
 struct ToDoItem: Identifiable { //동적 List구현 시 표시될 데이터는 Identifiable 프로토콜을 준수해야함.
     var id = UUID() //Identifiable프로토콜 준수하기위한 필수 프로퍼티 id. 인스턴스생성시 알아서 그 놈의 UUID값이 할당될 것임? 아마두!
     var task: String
@@ -54,3 +79,11 @@ struct ListView_Previews: PreviewProvider {
         ListView()
     }
 }
+
+/*
+ <편집 기능>
+ 1. 삭제
+ - dataSource에서 해당 항목을 삭제하는 onDelete() 수정자를 각 리스트 셀에 추가.
+ - 이 메서드가 호출되면 삭제될 행의 offset을 가진 IndexSet객체가 전달됨.
+ - 이 메서드를 구현했다면 사용자가 행을 스와이프할때 delete버튼이 뜨게됨.
+ */
